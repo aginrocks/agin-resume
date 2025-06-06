c285dc2c560a7de91ff84e1d519efb3148ceaf55
Tymek <vonautymek@gmail.com>
2025-06-06 12:46:10 +0200

    changed rendering method

--- components/editor/preview/index.tsx
+++ components/editor/preview/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect, useContext } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import { pdf } from '@react-pdf/renderer';
+import { BlobProvider, pdf } from '@react-pdf/renderer';
 import { Renderer } from '@components/renderer';
 
 // Import required CSS for react-pdf
@@ -24,34 +24,21 @@ export function Preview() {
 
     const form = useFormContext();
 
+    const [data, setData] = useState<z.infer<typeof resumeSchema>>(
+        form.getValues() as z.infer<typeof resumeSchema>
+    );
+
     useEffect(() => {
-        const generatePDF = async (values: z.infer<typeof resumeSchema>) => {
-            console.log('Generating PDF with values:', values);
-
-            try {
-                setLoading(true);
-                const blob = await pdf(<Renderer data={values} />).toBlob();
-                const url = URL.createObjectURL(blob);
-                setPdfBlob(url);
-            } catch (error) {
-                console.error('Error generating PDF:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
         const callback = form.subscribe({
             formState: {
                 values: true,
             },
             callback: ({ values }) => {
-                generatePDF(values as z.infer<typeof resumeSchema>);
+                setData(values as z.infer<typeof resumeSchema>);
             },
         });
 
         return () => callback();
-
-        // generatePDF();
     }, []);
 
     const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
@@ -62,37 +49,34 @@ export function Preview() {
         <div className="flex-1 w-full border-l border-sidebar-border bg-sidebar flex flex-col">
             <PreviewHeader />
             <div className="flex justify-center items-center flex-col gap-4 flex-1 px-4">
-                {loading ? (
-                    <div className="flex items-center justify-center h-full">
-                        <p>Generating PDF preview...</p>
-                    </div>
-                ) : pdfBlob ? (
-                    <Document
-                        file={pdfBlob}
-                        onLoadSuccess={onDocumentLoadSuccess}
-                        loading={
-                            <div className="flex items-center justify-center h-full">
-                                <p>Loading PDF...</p>
-                            </div>
-                        }
-                        className="flex flex-col items-center"
-                    >
-                        {Array.from(new Array(numPages), (el, index) => (
-                            <Page
-                                key={`page_${index + 1}`}
-                                pageNumber={index + 1}
-                                scale={1}
-                                className="mb-4 shadow-md dark:shadow-none rounded-md overflow-hidden"
-                                renderTextLayer={false}
-                                renderAnnotationLayer={false}
-                            />
-                        ))}
-                    </Document>
-                ) : (
-                    <div className="flex items-center justify-center h-full">
-                        <p>Failed to generate PDF preview</p>
-                    </div>
-                )}
+                <BlobProvider document={<Renderer data={data} />}>
+                    {({ blob, url, loading, error }) => {
+                        if (!blob || loading) return <p>Loading</p>;
+                        return (
+                            <Document
+                                file={blob}
+                                onLoadSuccess={onDocumentLoadSuccess}
+                                loading={
+                                    <div className="flex items-center justify-center h-full">
+                                        {/* <p>Loading PDF...</p> */}
+                                    </div>
+                                }
+                                className="flex flex-col items-center"
+                            >
+                                {Array.from(new Array(numPages), (el, index) => (
+                                    <Page
+                                        key={`page_${index + 1}`}
+                                        pageNumber={index + 1}
+                                        scale={1}
+                                        className="mb-4 shadow-md dark:shadow-none rounded-md overflow-hidden"
+                                        renderTextLayer={false}
+                                        renderAnnotationLayer={false}
+                                    />
+                                ))}
+                            </Document>
+                        );
+                    }}
+                </BlobProvider>
             </div>
         </div>
     );
