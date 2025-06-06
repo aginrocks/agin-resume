1364548335b7791dbbabd3af5fb6ef23e634b51c
Tymek <vonautymek@gmail.com>
2025-06-06 02:13:10 +0200

    added expand button

--- components/editor/core/index.tsx
+++ components/editor/core/index.tsx
@@ -1,14 +1,21 @@
-import { IconUser } from '@tabler/icons-react';
+import { IconAlignLeft, IconUser } from '@tabler/icons-react';
 import { Section } from './section';
 
 export function Core() {
     return (
-        <div className="flex-1 w-full bg-background rounded-2xl p-4">
-            <Section
-                title="Personal Data"
-                description="Your personal information"
-                icon={IconUser}
-            ></Section>
+        <div className="flex-1 w-full bg-background rounded-2xl">
+            <div className="flex flex-col p-4 gap-2.5">
+                <Section
+                    title="Personal Data"
+                    description="Your personal information"
+                    icon={IconUser}
+                ></Section>
+                <Section
+                    title="Summary"
+                    description="A brief summary of your experience and skills"
+                    icon={IconAlignLeft}
+                ></Section>
+            </div>
         </div>
     );
 }
--- components/editor/core/section.tsx
+++ components/editor/core/section.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@components/ui/card';
-import { Icon } from '@tabler/icons-react';
+import { Icon, IconChevronDown, IconChevronUp } from '@tabler/icons-react';
+import { useState } from 'react';
 
 export type SectionProps = {
     title: string;
@@ -9,14 +11,25 @@ export type SectionProps = {
 };
 
 export function Section({ title, description, icon: Icon, children }: SectionProps) {
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <div className="border rounded-md">
-            <div className="flex items-center gap-1.5 py-2.5 px-3">
-                {Icon && <Icon className="size-6 text-muted-foreground" />}
-                <div>
-                    <div className="font-semibold text-sm">{title}</div>
-                    <div className="text-muted-foreground text-xs">{description}</div>
+        <div className="border rounded-md cursor-pointer hover:bg-sidebar transition-colors">
+            <div className="flex justify-between items-center p-2.5 pl-3">
+                <div className="flex items-center gap-2">
+                    {Icon && <Icon className="size-5 text-muted-foreground" />}
+                    <div>
+                        <div className="font-semibold text-sm">{title}</div>
+                        <div className="text-muted-foreground text-xs">{description}</div>
+                    </div>
                 </div>
+                <Button size="icon" variant="ghost" onClick={() => setExpanded(!expanded)}>
+                    {expanded ? (
+                        <IconChevronUp className="size-4 text-muted-foreground" />
+                    ) : (
+                        <IconChevronDown className="size-4 text-muted-foreground" />
+                    )}
+                </Button>
             </div>
         </div>
     );
